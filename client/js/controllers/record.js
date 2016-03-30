c6e696e637e980c0840b0993cf6ba8726b5849db
Cristtopher Quintana T <cquintana@patagoniawings.com>
2016-03-30 15:25:26 -0300

    added filter

--- client/js/controllers/record.js
+++ client/js/controllers/record.js
@@ -1,13 +1,12 @@
 angular
   .module('app')
-  .controller('CDController', ['$scope', '$rootScope', function($scope, $rootScope) {
-      $scope.currentDate = new Date();
-      $rootScope.department = 'Angular';
-    }])
-
   .controller('RecordController', ['$scope', '$state', 'Record', function($scope,
       $state, Record) {
     $scope.records = [];
+    ONE_DAY = 24 * 60 * 60 * 1000;
+    ONE_WEEK = ONE_DAY * 7;
+    ONE_MONTH = ONE_WEEK * 4;
+    FILTER = '';
 
     function getRecords() {
         Record.find()
@@ -18,27 +17,74 @@ angular
     }
     getRecords();
 
-    var d = new Date();
-    var curr_date = d.getDate();
-    var curr_month = d.getMonth()+1;
-    var curr_year = d.getFullYear();
-    $scope.dateToday = Date.parse(curr_month + "/" + curr_date + "/" + curr_year);
-    $scope.dateRange = "";
+    var f=new Date();
+    var ano = f.getFullYear();
+    var mes = f.getMonth()+1;
+    var dia = f.getDate();
+
+    $scope.filterByDate = function(input){
+        return function(item){
+            var INPUT = new Date(item.input_datetime)
+            return INPUT.getTime() >= new Date(ano+"/"+mes+"/"+dia);
+        }
+    }
+
+    // Counts
+    $scope.inputs = Record.count({
+      where: { is_input: true }
+    });
+    $scope.permitted = Record.count({
+      where: { is_permitted : true }
+    });
+    $scope.rejected = Record.count({
+      where: { is_permitted : false }
+    });
+
+    // Paginate
+
+    //Reports
+    $scope.exportData = function () {
+        var blob = new Blob([document.getElementById('records').innerHTML], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+        });
+        saveAs(blob, "Report.xls");
+    };
 
     $scope.eventDateFilter = function(column) {
         console.log(column);
         if(column === 'today') {
-            $scope.dateRange = $scope.dateToday;
+            $scope.dateRange = "";
+            $scope.filterByDate = function(input){
+                return function(item){
+                    var INPUT = new Date(item.input_datetime)
+                    return INPUT.getTime() >= new Date(ano+"/"+mes+"/"+dia);
+                }
+            }
         } else if (column === 'pastWeek') {
             //need logic
+            //curr_date - 7 dias
+            $scope.dateRange = "";
+            $scope.filterByDate = function(input){
+                return function(item){
+                    var INPUT = new Date(item.input_datetime)
+                    return INPUT.getTime() >= Date.now()-ONE_WEEK;
+                }
+            }
         } else if (column === 'pastMonth') {
             //need logic
+            //curr_month - 1
+            $scope.filterByDate = function(input){
+                return function(item){
+                    var INPUT = new Date(item.input_datetime)
+                    return INPUT.getTime() >= Date.now()-ONE_MONTH;
+                }
+            }
         } else {
-            $scope.dateRange = "";
+            $scope.filterByDate = "";
         }
-        console.log($scope.dateRange);
     }
 
+    // New record, not in use now
     $scope.add = function() {
       Record
         .create($scope.newRecord)
@@ -51,8 +97,11 @@ angular
         });
     };
 
+    // Add comment to record
     $scope.update = function(record){
-  		record.$save();
+        record.updating = 1;
+        console.log(record);
+        record.$save(record);
   	};
 
   }]);
--- common/models/record.js
+++ common/models/record.js
@@ -20,36 +20,47 @@ module.exports = function(Record) {
                         };
                     });
 
-                    console.log(set_input);
-                    if(set_input){
-                        console.log("entrada");
-                        ctx.instance.is_input = true;
-                    }else{
-                        console.log("salida");
-                        ctx.instance.is_input = false;
-                    }
+                    if(set_input) ctx.instance.is_input = true;
+                    else ctx.instance.is_input = false;
                 }
             );
-        };
+        }
         next();
     });
 
     Record.observe('after save', function(ctx, next) {
         if (ctx.instance) {
             console.log(ctx.instance);
-            if(ctx.instance.is_input)
-                Record.updateAll({ id: ctx.instance.id }, { input_datetime: new Date() }, null);
-            else {
-                Record.updateAll({ id: ctx.instance.id_finded }, { output_datetime: new Date() }, null);
+            if(ctx.instance.is_input && ctx.instance.is_permitted &&
+            ctx.instance.updating == undefined){
+                console.log("Insert input");
+                Record.updateAll({ id: ctx.instance.id },
+                    { input_datetime: new Date(), is_input: true }, null);
+            }else if(ctx.instance.is_input &&
+            ctx.instance.updating == undefined){
+                console.log("Insert input with permision dennied");
+                Record.updateAll({ id: ctx.instance.id },
+                    { input_datetime: new Date() }, null);
+            }else if(ctx.instance.id_finded != undefined &&
+            ctx.instance.updating == undefined){
+                console.log("Insert output");
+                Record.updateAll({ id: ctx.instance.id_finded },
+                    { output_datetime: new Date(), is_input: false }, null);
                 Record.destroyById(ctx.instance.id, null);
+            }else if(ctx.instance.updating != undefined){
+                console.log("adding comment");
+                Record.updateAll({ id: ctx.instance.id },
+                    { comment: ctx.instance.comment }, null);
+                //Record.destroyById(ctx.instance.id, null);
             }
-        };
+        }
         next();
     });
 
 
     // Record.afterRemote('findById', function (ctx, result, next) {
-    //     result.updateAttribute({"$inc": {output_datetime: new Date()}}, function (err, instance) {
+    //     result.updateAttribute({"$inc": {output_datetime: new Date()}},
+    //     function (err, instance) {
     //         if(err) {console.log(err)};
     //         if(!err) {console.log(instance)}
     //     })
