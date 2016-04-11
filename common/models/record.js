707f515087e55df92037754842820552dc1ab34c
Cristtopher Quintana T <cquintana@patagoniawings.com>
2016-04-11 11:29:47 -0300

    change === by ==

--- common/models/record.js
+++ common/models/record.js
@@ -4,15 +4,13 @@ module.exports = function(Record) {
 
     Record.observe('before save', function(ctx, next) {
         if (ctx.instance) {
-            if(ctx.instance.is_input === undefined)
-                ctx.instance.is_input = true;
             Record.find({
                 where: { fullname: ctx.instance.fullname }},
                 function (err, records) {
                     var set_input = true;
                     records.forEach(function (record) {
-                        if(record.is_input == true &&
-                                record.output_datetime === undefined){
+                        if(record.is_input == true ||
+                                record.output_datetime == undefined){
                             set_input = false;
                             ctx.instance.id_finded = record.id;
                         }else{
@@ -31,33 +29,35 @@ module.exports = function(Record) {
     Record.observe('after save', function(ctx, next) {
         if (ctx.instance) {
             if(ctx.instance.is_input && ctx.instance.is_permitted &&
-                    ctx.instance.updating === undefined){
+                    ctx.instance.updating == undefined){
                 console.log("Insert input");
                 Record.updateAll({ id: ctx.instance.id },
                     { input_datetime: new Date(), is_input: true }, null);
             }else if(ctx.instance.is_input &&
                     ctx.instance.is_permitted == false &&
-                    ctx.instance.updating === undefined){
+                    ctx.instance.updating == undefined){
                 console.log("Insert input dennied");
                 Record.updateAll({ id: ctx.instance.id },
-                    { input_datetime: new Date(), is_input: true }, null);
+                    { input_datetime: new Date() }, null);
             }else if(ctx.instance.is_input == false &&
                     ctx.instance.id_finded !== undefined &&
-                    ctx.instance.updating === undefined){
+                    ctx.instance.updating == undefined){
                 console.log("Insert output");
                 Record.updateAll({ id: ctx.instance.id_finded },
-                    { output_datetime: new Date(), is_input: false }, null);
+                    { output_datetime: new Date() }, null);
+                console.log("Updated id: " + ctx.instance.id_finded);
                 Record.destroyById(ctx.instance.id, null);
+                console.log("Deleted id: " + ctx.instance.id);
             }else if(ctx.instance.is_input == false &&
-                    ctx.instance.id_finded === undefined &&
-                    ctx.instance.updating === undefined &&
+                    ctx.instance.id_finded == undefined &&
+                    ctx.instance.updating == undefined &&
                     ctx.instance.is_permitted){
                 console.log("Insert input (fixed)");
                 Record.updateAll({ id: ctx.instance.id },
-                    { input_datetime: new Date(), is_input: true }, null);
+                    { input_datetime: new Date() }, null);
             }else if(ctx.instance.is_input == false &&
-                    ctx.instance.id_finded === undefined &&
-                    ctx.instance.updating === undefined &&
+                    ctx.instance.id_finded == undefined &&
+                    ctx.instance.updating == undefined &&
                     ctx.instance.is_permitted){
                 console.log("Insert input dennnied (fixed)");
                 Record.updateAll({ id: ctx.instance.id },
@@ -70,8 +70,8 @@ module.exports = function(Record) {
                 console.log("else");
             }
 
-            if(ctx.instance.input_datetime === undefined &&
-                ctx.instance.output_datetime === undefined){
+            if(ctx.instance.input_datetime == undefined &&
+                ctx.instance.output_datetime == undefined){
                     ctx.instance.input_datetime = new Date();
                     console.log("Sin fecha de ingreso ni salida");
             }
