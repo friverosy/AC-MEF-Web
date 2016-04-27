b308f328ee514d3611e323d50e07478f4e753ee7
Cristtopher Quintana T <cquintana@patagoniawings.com>
2016-04-27 16:42:31 -0300

    improved find method on after save hook, fixes #10

---
+++ client/js/controllers/visits.js
@@ -0,0 +1,113 @@
+angular
+  .module('app')
+  .controller('VisitController', ['$scope', '$state', 'Record', function($scope,
+      $state, Record) {
+    $scope.records = [];
+    ONE_DAY = 24 * 60 * 60 * 1000;
+    ONE_WEEK = ONE_DAY * 7;
+    ONE_MONTH = ONE_WEEK * 4;
+    FILTER = '';
+
+    function getRecords() {
+        Record.find()
+        .$promise
+        .then(function(results) {
+            $scope.records = results;
+        });
+    }
+    getRecords();
+
+    var f=new Date();
+    var ano = f.getFullYear();
+    var mes = f.getMonth()+1;
+    var dia = f.getDate();
+
+    $scope.filterByDate = function(input){
+        return function(item){
+            var INPUT = new Date(item.input_datetime)
+            return INPUT.getTime() >= new Date(ano+"/"+mes+"/"+dia);
+        }
+    }
+
+    // Counts
+    $scope.inputs = Record.count({
+      where: { and:
+          [
+              {is_input: true},
+              {output_datetime: undefined},
+              {is_permitted: true}
+          ]
+      }
+    });
+    $scope.permitted = Record.count({
+      where: { is_permitted : true }
+    });
+    $scope.rejected = Record.count({
+      where: { is_permitted : false }
+    });
+
+    // Paginate
+
+    //Reports
+    $scope.exportData = function () {
+        var blob = new Blob([document.getElementById('records').innerHTML], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+        });
+        saveAs(blob, "Report.xls");
+    };
+
+    $scope.eventDateFilter = function(column) {
+        console.log(column);
+        if(column === 'today') {
+            $scope.dateRange = "";
+            $scope.filterByDate = function(input){
+                return function(item){
+                    var INPUT = new Date(item.input_datetime)
+                    return INPUT.getTime() >= new Date(ano+"/"+mes+"/"+dia);
+                }
+            }
+        } else if (column === 'pastWeek') {
+            //need logic
+            //curr_date - 7 dias
+            $scope.dateRange = "";
+            $scope.filterByDate = function(input){
+                return function(item){
+                    var INPUT = new Date(item.input_datetime)
+                    return INPUT.getTime() >= Date.now()-ONE_WEEK;
+                }
+            }
+        } else if (column === 'pastMonth') {
+            //need logic
+            //curr_month - 1
+            $scope.filterByDate = function(input){
+                return function(item){
+                    var INPUT = new Date(item.input_datetime)
+                    return INPUT.getTime() >= Date.now()-ONE_MONTH;
+                }
+            }
+        } else {
+            $scope.filterByDate = "";
+        }
+    }
+
+    // New record, not in use now
+    $scope.add = function() {
+      Record
+        .create($scope.newRecord)
+        .$promise
+        .then(function(record) {
+          $scope.newRecord = '';
+          $scope.recordForm.content.$setPristine();
+          $('.focus').focus();
+          getRecords();
+        });
+    };
+
+    // Add comment to record
+    $scope.update = function(record){
+        record.updating = 1;
+        console.log(record);
+        record.$save(record);
+  	};
+
+  }]);
