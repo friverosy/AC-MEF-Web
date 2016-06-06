6b2986aaff9520488e9865b157beb754080b8772
Cristtopher Quintana T <cquintana@patagoniawings.com>
2016-06-06 08:47:26 -0400

    added reports controller, still in develop

---
+++ client/js/controllers/reports.js
@@ -0,0 +1,18 @@
+angular
+  .module('app')
+  .controller('ReportsController', ['$scope', '$state', 'Record', '$http', '$window', function($scope,
+      $state, Record, $http, $window) {
+
+      if(localStorage.email != "cberzins@multiexportfoods.com" && localStorage.password != "CB3rZin5"){
+          $window.location.href = '/login';
+      }else{
+        //   console.log("aca");
+      }
+
+    $scope.logout = function() {
+          localStorage.clear();
+          $window.location.href = '/login';
+    };
+
+
+  }]);
