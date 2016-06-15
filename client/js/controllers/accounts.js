a7927e7575b8ce503faa202003d29b8942d6c82f
Cristtopher Quintana T <cquintana@patagoniawings.com>
2016-06-15 13:26:31 -0400

    added new users to login fake, jiji

--- client/js/controllers/accounts.js
+++ client/js/controllers/accounts.js
@@ -3,11 +3,34 @@ angular
   .controller('AccountsController', ['$scope', '$state', 'Record', '$http', '$window', function($scope,
       $state, Record, $http, $window) {
 
-      if(localStorage.email != "cberzins@multiexportfoods.com" && localStorage.password != "CB3rZin5"){
-          $window.location.href = '/login';
-      }else{
-        //   console.log("aca");
-      }
+    switch (localStorage.email) {
+      case "cberzins@multiexportfoods.com":
+        if (localStorage.password !== "CB3rZin5") $window.location.href = '/login';
+        break;
+        case "jaime":
+          if (localStorage.password !== "j4im3") $window.location.href = '/login';
+          break;
+        case "seguridad1":
+          if (localStorage.password !== "84799") $window.location.href = '/login';
+          break;
+        case "seguridad2":
+          if (localStorage.password !== "14551") $window.location.href = '/login';
+          break;
+        case "seguridad3":
+          if (localStorage.password !== "66494") $window.location.href = '/login';
+          break;
+        case "asistente1":
+          if (localStorage.password !== "25913") $window.location.href = '/login';
+          break;
+        case "asistente2":
+          if (localStorage.password !== "19825") $window.location.href = '/login';
+          break;
+        case "asistente3":
+          if (localStorage.password !== "41331") $window.location.href = '/login';
+          break;
+      default:
+        $window.location.href = '/login';
+    }
 
     $scope.logout = function() {
           localStorage.clear();
--- client/js/controllers/maintainers.js
+++ client/js/controllers/maintainers.js
@@ -4,6 +4,35 @@ angular
       $state, Parking, Destination) {
     $scope.parkings = [];
 
+    switch (localStorage.email) {
+      case "cberzins@multiexportfoods.com":
+        if (localStorage.password !== "CB3rZin5") $window.location.href = '/login';
+        break;
+        case "jaime":
+          if (localStorage.password !== "j4im3") $window.location.href = '/login';
+          break;
+        case "seguridad1":
+          if (localStorage.password !== "84799") $window.location.href = '/login';
+          break;
+        case "seguridad2":
+          if (localStorage.password !== "14551") $window.location.href = '/login';
+          break;
+        case "seguridad3":
+          if (localStorage.password !== "66494") $window.location.href = '/login';
+          break;
+        case "asistente1":
+          if (localStorage.password !== "25913") $window.location.href = '/login';
+          break;
+        case "asistente2":
+          if (localStorage.password !== "19825") $window.location.href = '/login';
+          break;
+        case "asistente3":
+          if (localStorage.password !== "41331") $window.location.href = '/login';
+          break;
+      default:
+        $window.location.href = '/login';
+    }
+
     function getParkings() {
         Parking.find()
         .$promise
