464bf3291403631e63cc16ec3fe7dfef85786b21
Cristtopher Quintana T <cquintana@patagoniawings.com>
2016-09-20 10:01:57 -0300

    people create through promises

--- common/models/people.js
+++ common/models/people.js
@@ -1,96 +1,105 @@
+var app = require('../../server/server')
+
 module.exports = function(People) {
     // remove the DELETE functionality from API
     People.disableRemoteMethod('deleteById', true);
     var Promise = require('bluebird');
-
-    People.getFromXml = function(profile, cb) {
-      var fs = require('fs')
-      var parseString = require('xml2js').parseString;
-      var xml2js = require('xml2js');
-      var parser = new xml2js.Parser(xml2js.defaults["0.2"]);
-
-      fs.readFile(__dirname + '/XML_EMPLEADOS.xml', function(err, data) {
-        if (err) {
-          console.log(new Date(), "Archivo XML_EMPLEADOS no econtrado");
-        } else {
-          parser.parseString(data, function (err, result) {
-            for (var i in result.EMPLEADOS_MEF.EMPLEADO) {
-              result.EMPLEADOS_MEF.EMPLEADO[i].profile = "E";
-              result.EMPLEADOS_MEF.EMPLEADO[i].is_permitted = true;
-              destroyPeople(profile)
-              .then(content => upsertPeople(result.EMPLEADOS_MEF.EMPLEADO[i]))
-              .catch(err => console.log(err.message))
-              //dell
-              //upsert
-            };
-            // updatePeople(profile, result.EMPLEADOS_MEF.EMPLEADO);
-            cb(null, 201);
-          });
-        }
-      });
-    }
+    var fs = require('fs');
 
     function destroyPeople(profile) {
-      People.destroyAll({profile: profile}, function(err, info, cnt){
-        if(err) console.log(err);
-        else {
-          console.log(info);
-          console.log(cnt);
-          return Promise.resolve(info);
-        }
+      return new Promise(function (resolve, reject) {
+        People.destroyAll({profile: "E"}, function(err, info){
+          if (err) {
+            reject(err)
+          } else {
+            console.log("People destroyed ", info);
+            resolve("E")
+          }
+        })
       })
     }
 
-    function upsertPeople(person){
-      People.upsert(person, function(err, obj) {
-        if (err) { console.log(new Date(), err) }
-      });
+    function insertPerson(content) {
+      return new Promise(function (resolve, reject) {
+        var parseString = require('xml2js').parseString;
+        var xml2js = require('xml2js');
+        var parser = new xml2js.Parser(xml2js.defaults["0.2"]);
+
+        var profile = "E";
+        if (profile === "E") {
+          xml = __dirname + "/XML_EMPLEADOS.xml";
+        } else if (profile === "C") {
+          xml = __dirname + "/XML_SUBCONTRATISTAS.xml";
+        }
+        fs.readFile(xml, function (err, content) {
+          if (err) {
+            return reject(err)
+          }
+          parser.parseString(content, function (err, result) {
+            for (var i in result.EMPLEADOS_MEF.EMPLEADO) {
+              result.EMPLEADOS_MEF.EMPLEADO[i].profile = "E";
+              result.EMPLEADOS_MEF.EMPLEADO[i].is_permitted = true;
+              var location = result.EMPLEADOS_MEF.EMPLEADO[i].location + '';
+              var plant = location.split("-");
+              upsertModel("Destination", location, null)
+              upsertModel("Company", result.EMPLEADOS_MEF.EMPLEADO[i].company, result.EMPLEADOS_MEF.EMPLEADO[i].company_code)
+              upsertModel("ProcessPlant", plant[0], null)
+              People.create(result.EMPLEADOS_MEF.EMPLEADO[i], function(err, obj) {
+                if (err) {
+                  return reject(err)
+                }
+              })
+            }
+          })
+          resolve()
+        })
+      })
     }
 
-    function updatePeople(profile, xml){
-      People.find({where: {profile: profile}}, function(err, people) {
-        for (var p in people) {
-          console.log(xml.indexOf(people[p].run));
-          // if (xml.indexOf(people[p].run) === -1) {
-          //   console.log("ya existe ", people[p].run);
-          // } else if (xml.indexOf(people[p].run) > -1){
-          //   console.log("eliminar ", people[p].run);
-          // }
-        }
-      });
+    function upsertModel(model, name, rut){
+      switch (model) {
+        case "Company":
+          var Company = app.models.Company
+          Company.upsert(
+            {name: name, rut: rut},
+            function(err, instance, created) {
+              if (err) console.log(err)
+            }
+          )
+          break;
+        case "Destination":
+          var Destination = app.models.Destination
+          Destination.upsert(
+            {name: name},
+            function(err, instance, created) {
+              if (err) console.log(err)
+            }
+          )
+          break;
+        case "ProcessPlant":
+          var ProcessPlant = app.models.ProcessPlant
+          ProcessPlant.upsert(
+            {name: name},
+            function(err, instance, created) {
+              if (err) reject(err)
+            }
+          )
+          break;
+      }
+    }
 
-      // People.find({where: {profile: profile}}, function(err, people) {
-        //
-      //   for (var p in people) {
-      //     for (var x in xml) {
-      //       var finded;
-      //       // console.log(xml[x].run + " -> " + people[p].run);
-      //       if (xml[x].run == people[p].run)
-      //         finded = true;
-      //       else {
-      //         finded = false;
-      //       }
-      //       console.log(finded);
-      //       if (finded == false) {
-      //         //console.log("eliminar ", people[p].run);
-      //         People.destroyById(people[p].run, function(err){
-      //           if(err) console.log(err);
-      //           else {
-      //             console.log("eliminada!");
-      //           }
-      //         })
-      //       }
-      //     }
-      //   }
-      //   console.log("done");
-      // })
+    People.getFromXml = function(cb) {
+      destroyPeople()
+      .then(profile => insertPerson(profile))
+      .catch(err => cb(null, err))
+      cb(null, 201)
     }
 
     People.remoteMethod(
       'getFromXml',
       {
-        accepts: {arg: 'profile', type: 'string', required: true},
-        returns: {arg: 'status', type: 'string'}
+        returns: {arg: 'status', type: 'string'},
+        http: {path: '/updateDbFromXml', verb: 'get'}
       }
     );
 
@@ -121,7 +130,8 @@ module.exports = function(People) {
       'getLastUpdate',
       {
         accepts: {arg: 'profile', type: 'string', required: true},
-        returns: {arg: 'datetime', type: 'string'}
+        returns: {arg: 'datetime', type: 'string'},
+        http: {path: '/getLastUpdate', verb: 'get'}
       }
     );
 };
